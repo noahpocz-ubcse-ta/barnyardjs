a74d9e20e9b504152cfded50e22c61b09d6dc30d
Omar Radwan <omarradw@buffalo.edu>
2017-11-04 21:08:32 -0400

    add animation

--- src/App.js
+++ src/App.js
@@ -3,17 +3,25 @@ import logo from './logo.svg';
 import './App.css';
 
 import Critter from './components/critter';
+import critterImage from "./components/spritesheets/butterfly_right.png"
 
 class App extends Component {
+
+  componentDidMount() {
+    this.critter = new Critter('butterfly');
+    this.critter.startTimer();
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          <Critter />
+        <canvas id="barnyard" width = "1000" height = "1000"></canvas>
+        <img id="source" alt='me' src={critterImage} style={{visibility:"hidden"}}/>
         </p>
       </div>
     );
--- src/components/critter.js
+++ src/components/critter.js
@@ -1,31 +1,60 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
 
-class Critter extends Component {
+class Critter {
 
-    constructor(props) {
-        super(props);
+    constructor(animalType) {
 
-        this.state = {timer: null, counter: 0}
+        this.timer = null;
+        this.counter = 0;
         this.tick = this.tick.bind(this);
+        this.draw = this.draw.bind(this);
+
+        if (animalType == 'pig') {
+            this.sx=3;
+            this.sy=6;
+            this.sw=83;
+            this.sh=54;
+            this.dx=3;}
+        if(animalType=='butterfly'){
+        this.sx=15;
+        this.sy=18;
+        this.sw=70;
+        this.sh=50;
+        this.dx=15;}
+        if(animalType=='chicken'){
+            this.sx=0;
+            this.sy=7;
+            this.sw=36;
+            this.sh=63;
+            this.dx=0;}
     }
 
-    componentDidMount() {
-        let timer = setInterval(this.tick, 1000);
-        this.setState({timer});
+    startTimer() {
+        this.timer = setInterval(this.tick, 62.5);
     }
 
     tick() {
-        this.setState({
-            counter: this.state.counter + 1
-        })
+        this.counter += 1
+        this.sx+=this.sw;
+        this.dx+=this.sw;
+        if(this.sx==925){
+        this.sx=15;
+        this.dx=15;}
+        if(this.sx==618)
+        {this.sx=3;
+        this.dx=3}
+        if(this.sx==373)
+        {this.sx==0;
+        this.dx==0;}
+        this.draw();
     }
 
-    render() {
-        
-        return (
-            <span> Here's a counter: {this.state.counter} </span>
-        );
-
+    draw() {
+        var canvas = document.getElementById('barnyard');
+        var ctx = canvas.getContext('2d');
+        var critterImage = document.getElementById('source');
+        ctx.clearRect(0,0,1000,1000);
+        ctx.drawImage(critterImage,this.sx,this.sy,this.sw,this.sh,this.dx,200,this.sw,this.sh);
     }
 }
 
