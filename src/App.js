0f3e335b15d1b0883d7b536ad9638504895ebf2d
noahpoz <noahpoczciwinski@gmail.com>
2017-11-07 01:22:48 -0500

    background image

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { Card } from 'material-ui/Card';
+
 import Butterly from './critters/butterfly';
 import Chicken from './critters/chicken';
 import Pig from './critters/pig';
 
+import terrainImage from "./Terrain.jpg"
 import butterflyImage from "./critters/spritesheets/butterfly_right.png"
 import chickenImage from "./critters/spritesheets/chicken_walk_right.png"
 import pigImage from "./critters/spritesheets/pig_walk_right.png"
@@ -33,11 +36,16 @@ class App extends Component {
     this.critters.push(new Butterly());
     this.critters.push(new Pig());
 
-    this.timer = setInterval(this.tick, 62.5);
+    const img = document.getElementById("terrain-image");
+    img.onload = (event) => {
+      this.backgroundImage = event.target
+      this.timer = setInterval(this.tick, 62.5);
+    }
   }
 
   tick() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.ctx.drawImage(this.backgroundImage, 0, 0, this.canvas.width, this.canvas.height);
     this.critters.forEach(elem => {
       elem.draw();
     });
@@ -46,11 +54,18 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <img id="butterfly-image" alt='me' src={butterflyImage} style={ srcImageStyle } />
-        <img id="chicken-image" alt='me' src={chickenImage} style={ srcImageStyle } />
-        <img id="pig-image" alt='me' src={pigImage} style={ srcImageStyle } />
+        <div style={{ paddingLeft: '20px', paddingRight: '20px' }}>
 
-        <canvas id="barnyard" width = "1000" height = "1000" />
+          <img id="terrain-image" alt='terrain' src={terrainImage} style={srcImageStyle} />
+          <img id="butterfly-image" alt='me' src={butterflyImage} style={ srcImageStyle } />
+          <img id="chicken-image" alt='me' src={chickenImage} style={ srcImageStyle } />
+          <img id="pig-image" alt='me' src={pigImage} style={ srcImageStyle } />
+
+          <Card>
+            <canvas id="barnyard" width = "500" height = "300" />
+          </Card>
+
+        </div>
       </div>
     );
   }
